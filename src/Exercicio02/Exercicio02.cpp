72f8457315f5925670dda2563fedb5c0f2b9989b
Yan Kelvin <yan.kelvin@hotmail.com>
2020-02-04 09:59:57 -0300

    Ajuste Exercicio 02

--- src/Exercicio02/Exercicio02.cpp
+++ src/Exercicio02/Exercicio02.cpp
@@ -4,34 +4,31 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "pt-Br");
+	setlocale(LC_ALL, "pt-Br");
 
-    int a[5];
-    int b[5];
-    int soma = 0;
+	int a[5];
+	int b[5];
+	int soma = 0;
 
-    int tamanho = sizeof a / sizeof a[0];
+	int tamanho = sizeof a / sizeof a[0];
 
-    for (int i = 0; i < tamanho; i++)
-    {
-        cout << "Digite o " << i + 1 << "ª elemento de A: ";
-        cin >> a[i];
-    }
+	for (int i = 0; i < tamanho; i++)
+	{
+		cout << "Digite o " << i + 1 << "ª elemento de A: ";
+		cin >> a[i];
+	}
 
-    cout << endl;
+	cout << endl;
 
-    for (int i = 0; i < tamanho; i++)
-    {
-        cout << "Digite o " << i + 1 << "ª elemento de B: ";
-        cin >> b[i];
-    }
+	for (int i = 0; i < tamanho; i++)
+	{
+		cout << "Digite o " << i + 1 << "ª elemento de B: ";
+		cin >> b[i];
 
-    cout << endl;
+		soma += a[i] * b[i];
+	}
 
-    for (int i = 0; i < tamanho; i++)
-    {
-        soma += a[i] * b[i];
-    }
+	cout << endl;
 
-    cout << "O produto escalar de a[] por b[] é " << soma;
+	cout << "O produto escalar de a[] por b[] é " << soma;
 }
